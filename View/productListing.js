e920294768cdaa41a66597ba5727f7804589bd7d
Andeskjerf <predgd@pm.me>
2024-04-15 10:42:56 +0200

    make filter functional

--- View/productListing.js
+++ View/productListing.js
@@ -1,22 +1,40 @@
+function getToppings(item) {
+  let toppings = []
+  model.data.toppings.forEach((e) => {
+    if (item.productType == e.productType) toppings.push(e)
+  })
+  return toppings
+}
+
 function showListings() {
-    let html = ``
-    model.data.items.forEach((e) => {
-      if (e.category == model.input.start.category) html += listing(e)
+  let html = ``
+  model.data.items.forEach((e) => {
+    let hasFilter = false
+    let toppings = getToppings(e)
+    toppings.forEach((topping) => {
+      topping.filters.forEach((f) => {
+        if (model.input.start.filter.includes(f)) {
+          hasFilter = true
+        }
+      })
     })
-  
-    if (html == ``) {
-      html += `
+    if (e.category == model.input.start.category && !hasFilter)
+      html += listing(e)
+  })
+
+  if (html == ``) {
+    html += `
               <span>
                   Ingen produkter funnet
               </span>
           `
-    }
-  
-    return html
   }
-  
-  function listing(item) {
-    return `
+
+  return html
+}
+
+function listing(item) {
+  return `
           <div>
               <p>Name: ${item.name}</p>
               <p>Price: ${item.price}</p>
@@ -25,4 +43,4 @@ function showListings() {
               <br>
           </div>
       `
-  }
+}
