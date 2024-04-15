3a109efb9b3ff5bfffd39e58aefe2e7905d29952
sebgun123 <sebastiangun@getacademy.no>
2024-04-15 14:54:10 +0200

    highlight function
    laget en function for å vise hvilken items er highlightet og at de kommer på top av listen med gul backgrund.

--- View/productListing.js
+++ View/productListing.js
@@ -7,40 +7,65 @@ function getToppings(item) {
 }
 
 function showListings() {
-  let html = ``
+  let highlightedHtml = '';
+  let normalHtml = '';
+
+  Highlight(); 
+
   model.data.items.forEach((e) => {
-    let hasFilter = false
-    let toppings = getToppings(e)
+    let hasFilter = false;
+    let toppings = getToppings(e);
     toppings.forEach((topping) => {
       topping.filters.forEach((f) => {
         if (model.input.start.filter.includes(f)) {
-          hasFilter = true
+          hasFilter = true;
         }
-      })
-    })
-    if (e.category == model.input.start.category && !hasFilter)
-      html += listing(e)
-  })
+      });
+    });
+    
+    if (e.category === model.input.start.category && !hasFilter) {
+      const itemHtml = listing(e);
+      if (model.data.highlightedItems.includes(e.id)) {
+        highlightedHtml += itemHtml;
+      } else {
+        normalHtml += itemHtml;
+      }
+    }
+  });
 
-  if (html == ``) {
+  let html = highlightedHtml + normalHtml;
+
+  if (html === '') {
     html += `
-              <span>
-                  Ingen produkter funnet
-              </span>
-          `
+      <span>
+        Ingen produkter funnet
+      </span>
+    `;
   }
 
-  return html
+  return html;
 }
 
 function listing(item) {
+  const highlightedClass = model.data.highlightedItems.includes(item.id) ? 'highlighted' : '';
+  
   return `
-          <div>
-              <p>Name: ${item.name}</p>
-              <p>Price: ${item.price}</p>
-              <img src=${item.image} />
-              <p>Options: ${item.options}</p>
-              <br>
-          </div>
-      `
+    <div id="item-${item.id}" class="${highlightedClass}">
+      <p>Name: ${item.name}</p>
+      <p>Price: ${item.price}</p>
+      <img src="${item.image}" />
+      <p>Options: ${item.options}</p>
+      <br>
+    </div>
+  `;
+}
+
+function Highlight() {
+  for (let id of model.data.highlightedItems) {
+    for (let item of model.data.items) {
+      if (id === item.id) {
+        const itemElement = document.getElementById(`item-${id}`);
+      }
+    }
+  }
 }
