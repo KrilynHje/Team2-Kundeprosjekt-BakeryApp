d7fc04f5816d7e772306acb61c09793af3194fce
Kristian Hjelset <157597213+KrilynHje@users.noreply.github.com>
2024-04-29 14:18:35 +0200

    Co-authored-by: Marius Aalto <aaltofar@users.noreply.github.com>

--- View/toppingsView.js
+++ View/toppingsView.js
@@ -2,12 +2,18 @@ function toppingsView() {
     let html = ""
     html += /*HTML*/
         `
-   
         <div class="page">
-
         ${showHeader()}
+
+
+        <button onclick="addOrRemoveCount('-')">-</button>
+        <button onclick="addOrRemoveCount('+')">+</button>
     
     </div>
     `
     return html
+}
+
+function addOrRemoveCount(operator){
+    if(model.inputs.toppingsCount == 0) return
 }
