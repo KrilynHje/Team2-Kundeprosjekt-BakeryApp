3338af59cc39adf3b79bb94a2d8f5c2b306656bb
Kristian Hjelset <157597213+KrilynHje@users.noreply.github.com>
2024-05-07 09:41:21 +0200

    Co-authored-by: Marius Aalto <aaltofar@users.noreply.github.com> Co-authored-by: Andeskjerf <Andeskjerf@users.noreply.github.com> Co-authored-by: William <xEdgeLordx@users.noreply.github.com>

--- View/basketView.js
+++ View/basketView.js
@@ -19,6 +19,39 @@ function basketView() {
 
 }
 
+// {
+//     itemId: item.id,
+//     count: count,
+//     selectedTopping: [
+//            {
+//                  id: 0,
+//                  count: 1,
+//            }
+//        ],
+//     comment: "",
+// }
+// 🏴‍☠️🏴‍☠️🏋️🏴‍☠️🦜
+// function testFind(arr){
+//     let result = []
+//     for (let i = 0; i < arr.length; i++) {
+//         const el = arr[i];
+//         result.push([...model.data.toppings].find((t) => t.id == el.id))
+//     }
+//     return result
+// }
+
+function showToppingInfo(toppings) {
+    let html = ``
+
+    toppings.forEach((e) => {
+        let topping = model.data.toppings.find((t) => t.id == e.id)
+        if (e.count > 0)
+            html += `<p>${topping.name}, x ${e.count}</p>`
+    })
+
+    return html
+}
+
 function drawCart() {
 
     let cartHtml = "";
@@ -27,20 +60,19 @@ function drawCart() {
     for (counter = 0; counter < model.input.basket.length; counter++) {
 
         let item = finditem(model.input.basket[counter].itemId);
+        // let toppings = testFind(model.input.basket[counter].selectedTopping)
         let toppings = findtopping(model.input.basket[counter].itemId);
         let count = model.input.basket[counter].count
 
         cartHtml += /*html*/ `
-        <div class = "item-container"><div>Navn: ${item.name} </div>
+        <div class = "item-container"><div> ${item.name} </div>
 
         <img class = "image-container" src = "${item.image}"/>
 
-        <div>
-        ${toppings.name} Antall: <button onclick = "decreaseItemCount(${counter})">-</button>
-        
-        ${count}
+        ${showToppingInfo(model.input.basket[counter].selectedTopping)}
 
-        <button onclick = "increaseItemCount(${counter})">+</button>
+        <div>
+        ${toppings.name} Antall: <button class="topping-button" onclick = "decreaseItemCount()">-</button> ${counter}<button class="topping-button" onclick = "increaseItemCount()">+</button>
 
         <div>Pris ${item.price * count + toppings.price * count}Kr</div></div>
         `
